5dda6673262587396c707070b5991819aef81f93
Арсений Сендецкий <ceh938of@yandex.ru>
2021-12-22 03:19:13 +0800

    область гравитона

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -95,8 +95,14 @@ namespace kurs
 
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            emitter.MousePositionX = e.X;
-            emitter.MousePositionY = e.Y;
+
+            foreach (var emitter in emitters)
+            {
+                emitter.MousePositionX = e.X;
+                emitter.MousePositionY = e.Y;
+            }
+            point2.X = e.X;
+            point2.Y = e.Y;
         }
 
         private void tbDirection_Scroll(object sender, EventArgs e)
