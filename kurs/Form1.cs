a2b6cf6919d97f8d8764270e1639b81775bbaa83
Арсений Сендецкий <ceh938of@yandex.ru>
2021-12-22 01:03:10 +0800

    убираем взрыв в начале + движение мыши

--- kurs/Form1.cs
+++ kurs/Form1.cs
@@ -19,15 +19,6 @@ namespace kurs
 
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
 
-            for (var i = 0; i < 500; ++i)
-            {
-                var particle = new Particle();
-                // переношу частицы в центр изображения
-                particle.X = picDisplay.Width / 2;
-                particle.Y = picDisplay.Height / 2;
-                // добавляю в список
-                particles.Add(particle);
-            }
         }
 
         // добавил функцию обновления состояния системы
@@ -35,9 +26,41 @@ namespace kurs
         {
             foreach (var particle in particles)
             {
-                var directionInRadians = particle.Direction / 180 * Math.PI;
-                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
-                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.Direction = Particle.rand.Next(360);
+                    particle.Speed = 1 + Particle.rand.Next(10);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                    // перемещаю частицу в центр
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                }
+                else
+                {
+                    // а это наш старый код
+                    var directionInRadians = particle.Direction / 180 * Math.PI;
+                    particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                    particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+                }
+            }
+
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new Particle();
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
             }
         }
 
@@ -63,6 +86,14 @@ namespace kurs
             }
 
             picDisplay.Invalidate();
+        }
+
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            MousePositionX = e.X;
+            MousePositionY = e.Y;
         }
     }
 }
--- kurs/Particle.cs
+++ kurs/Particle.cs
@@ -16,6 +16,8 @@ namespace kurs
         public float Direction;
         public float Speed;
 
+        public float Life; // запас здоровья частицы
+
         public static Random rand = new Random();
 
         public Particle()
@@ -23,15 +25,24 @@ namespace kurs
             Direction = rand.Next(360);
             Speed = rand.Next(10);
             Radius = 2 + rand.Next(10);
+            Life = 20 + rand.Next(100);
         }
 
 
         public void Draw(Graphics g)
         {
-            var b = new SolidBrush(Color.Black);
+            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
+            float k = Math.Min(1f, Life / 100);
+            // рассчитываем значение альфа канала в шкале от 0 до 255
+            // по аналогии с RGB, он используется для задания прозрачности
+            int alpha = (int)(k * 255);
+
+            var color = Color.FromArgb(alpha, Color.Black);
+            var b = new SolidBrush(color);
 
             // нарисовали залитый кружок радиусом Radius с центром в X, Y
             g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+            
             // удалили кисть из памяти
             b.Dispose();
         }
